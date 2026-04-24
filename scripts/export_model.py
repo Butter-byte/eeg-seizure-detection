466b3cbed5a013c1f43f3c50b908080c1edf50b6
harsh-bothra2007 <harshbothra2504@gmail.com>
2026-04-24 21:51:57 +0530

    updated export script

--- scripts/export_model.py
+++ scripts/export_model.py
@@ -3,40 +3,28 @@ import os
 import warnings
 import logging
 
-# -----------------------------
-# Fix import path (VERY IMPORTANT)
-# -----------------------------
+# Fix import path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-# -----------------------------
 # Silence logs
-# -----------------------------
 warnings.filterwarnings("ignore")
 logging.getLogger("torch.onnx").setLevel(logging.ERROR)
-os.environ["TORCH_LOGS"] = "error"
 os.environ["PYTHONWARNINGS"] = "ignore"
 
-# -----------------------------
-# Imports
-# -----------------------------
 import torch
 from torchinfo import summary
 from src.models.maven_net import MavenNet
 
 
-# -----------------------------
-# MAIN
-# -----------------------------
 if __name__ == "__main__":
 
-    device = torch.device("cpu")  # keep CPU for export stability
+    device = torch.device("cpu")
 
     model = MavenNet().to(device)
     model.eval()
 
     print("\nMODEL SUMMARY:\n")
 
-    # 🔥 IMPORTANT: match your training input (scales=32)
     summary(
         model,
         input_size=(1, 1, 178, 32),
@@ -44,12 +32,8 @@ if __name__ == "__main__":
         device="cpu"
     )
 
-    # Dummy input
     dummy_input = torch.randn(1, 1, 178, 32).to(device)
 
-    # -----------------------------
-    # ONNX EXPORT (CLEAN)
-    # -----------------------------
     torch.onnx.export(
         model,
         dummy_input,
@@ -60,9 +44,8 @@ if __name__ == "__main__":
             "Input": {0: "batch_size"},
             "Output": {0: "batch_size"}
         },
-        opset_version=17,   # stable version
+        opset_version=17,
         verbose=False
     )
 
     print("\nONNX model exported as MavenNet.onnx")
-    print("Open it in https://netron.app")
